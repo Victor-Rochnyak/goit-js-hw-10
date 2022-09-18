f2195ceaba373ea3eae0e9048def704436af65e5
Victor-Rochnyak <victor.rochnyak@gmail.com>
2022-09-18 17:32:36 +0300

    DZ

--- src/index.js
+++ src/index.js
@@ -49,9 +49,8 @@ import './css/styles.css';
      // выполни санитизацию введенной строки методом trim()
      const boxValue =  refs.searchBox.value;
       const searchBoxValue = boxValue.trim();
- if (searchBoxValue === '') {
-     refs.countryList.innerHTML = '';
-     refs.countryInfo.innerHTML = '';
+ if  (!searchBoxValue) {
+      return;
  }
  // вызываем фукнция при сабмите на импут и обрабатываем ошибку 
      fetchCountry(searchBoxValue)
@@ -60,7 +59,8 @@ import './css/styles.css';
          Notify.failure('Oops, there is no country with that name')
      });
  }
- 
+
+
  //ф-ция подбора значений + интерфейс
    function renderCountryCard(countrys) {
      if (countrys.length >= 1 && countrys.length < 10) {
@@ -76,5 +76,124 @@ import './css/styles.css';
     if (countrys.length >= 10) {
      Notify.info('Too many matches found. Please enter a more specific name.');
     }
-    
- }
+  };
+
+
+
+
+
+//  }
+// import './css/styles.css';
+// import { fetchCountries } from './js/api';
+// //  import {fetchCountry} from './js/api.js';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import debounce from 'lodash.debounce';
+
+// const DEBOUNCE_DELAY = 300;
+// const refs = {
+//   inputBox: document.querySelector('#search-box'),
+//   countriList: document.querySelector('.country-list'),
+//   countriInfo: document.querySelector('.country-info'),
+// };
+
+// refs.inputBox.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+// function onInputChange(e) {
+//   const countryName = e.target.value.trim();
+
+//   if (!countryName) {
+//     clearTemplate();
+//     return;
+//   }
+
+//   fetchCountries(countryName)
+//     .then(data => {
+//       if (data.length > 10) {
+//         specificNameInfo();
+//         clearTemplate();
+//         return;
+//       }
+//       renderTemplate(data);
+//     })
+//     .catch(error => {
+//       clearTemplate();
+//       errorWarn();
+//     });
+// }
+
+// function errorWarn() {
+//     Notify.failure(`Oops, there is no country with that name`);
+//   }
+
+//   function specificNameInfo() {
+//     Notify.info(`Too many matches found. Please enter a more specific name.`);
+//   }
+
+// function clearTemplate() {
+//     refs.countriInfo.innerHTML = '';
+//     refs.countriList.innerHTML = '';
+//   }
+
+// function renderTemplate(elements) {
+//   let template = '';
+//   let refsTemplate = '';
+//   clearTemplate();
+
+//   if (elements.length === 1) {
+//     template = createItem(elements);
+//     refsTemplate = refs.countriInfo;
+//   } else {
+//     template = createList(elements);
+//     refsTemplate = refs.countriList;
+//   }
+
+//   drawTemplate(refsTemplate, template);
+// }
+
+// function createItem(element) {
+//   return element.map(
+//     ({ name, capital, population, flags, languages }) =>
+//       `
+//       <img
+//         src="${flags.svg}" 
+//         alt="${name.official}" 
+//         width="80" 
+//         height="50">
+//       <h1 class="country-info__title">${name.official}</h1>
+//       <ul class="country-info__list">
+//           <li class="country-info__item">
+//           <span>Capital:</span>
+//         ${capital}
+//           </li>
+//           <li class="country-info__item">
+//           <span>Population:</span>
+//           ${population}
+//           </li>
+//           <li class="country-info__item">
+//           <span>Lenguages:</span>
+//           ${Object.values(languages)}
+//           </li>
+//       </ul>
+//   `
+//   );
+// }
+
+// function createList(elements) {
+//   return elements
+//     .map(
+//       ({ name, flags }) => `
+//       <li class="country-list__item">
+//         <img class="country-list__img" 
+//           src="${flags.svg}" 
+//           alt="${name.official}" 
+//           width="50" 
+//           height="50">
+//         ${name.official}
+//       </li>`
+//     )
+//     .join('');
+// }
+
+// function drawTemplate(refs, markup) {
+//   refs.innerHTML = markup;
+// }
--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,4 @@
- //ф-ция поиска из библиотеки
+//  ф-ция поиска из библиотеки
   export function fetchCountry(name) {  
     return fetch(
       `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`,
@@ -9,3 +9,20 @@
           return response.json();
         });
 }
+
+
+
+// const fetchCountries = function (name) {  
+//   return fetch(
+//     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`,
+//   )
+//   .then(
+//     response => {
+//       if (response.status === 404) {
+//         return Promise.reject(new Error());
+//       }
+//       return response.json();
+//       });
+// }  
+
+// export { fetchCountries };
